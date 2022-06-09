4c3ecc5d3b998c7fa9a7077087ca59a6701ed8b1
Amin <mohammadazimi16@gmail.com>
2022-06-09 16:27:06 +0300

    initial commit

---
+++ src/index.ts
@@ -0,0 +1,34 @@
+import { MikroORM } from "@mikro-orm/core";
+import { ApolloServer } from "apollo-server-express";
+import express from "express";
+import 'reflect-metadata';
+import { buildSchema } from "type-graphql";
+import microConfig from "./mikro-orm.config";
+import { PostResolver } from "./resolvers/post";
+
+const main = async () => {
+  const orm = await MikroORM.init(microConfig);
+  orm.getMigrator().up();
+
+  const app = express();
+
+  const apolloServer = new ApolloServer({
+    schema: await buildSchema({
+      resolvers: [PostResolver],
+      validate: false,
+    }),
+    context: () => ({ em: orm.em }),
+  });
+
+  await apolloServer.start();
+
+  apolloServer.applyMiddleware({ app });
+
+  app.listen(4000, () => {
+    console.log("listening on port 4000");
+  });
+};
+
+main().catch((err) => {
+  console.error(err);
+});
---
+++ src/resolvers/post.ts
@@ -0,0 +1,48 @@
+import { FilterQuery, RequiredEntityData } from '@mikro-orm/core';
+import { MyContext } from 'src/types';
+import { Arg, Ctx, Int, Mutation, Query, Resolver } from 'type-graphql';
+import { Post } from '../entities/Post';
+
+@Resolver()
+export class PostResolver {
+  @Query(() => [Post])
+  posts(@Ctx() { em }: MyContext): Promise<Post[]> {
+    return em.find(Post, {});
+  }
+
+  @Query(() => Post, { nullable: true })
+  post(
+    @Arg('id', () => Int) id: number,
+    @Ctx() { em }: MyContext
+  ): Promise<Post | null> {
+    return em.findOne(Post, { id });
+  }
+
+  @Mutation(() => Post)
+  async createPost(
+    @Arg('title') title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = em.create(Post, { title } as RequiredEntityData<Post>);
+    await em.persistAndFlush(post);
+    return post;
+  }
+
+  @Mutation(() => Post)
+  async updatePost(
+    @Arg('id', { nullable: true }) id: number,
+    @Arg('title') title: string,
+    @Ctx() { em }: MyContext
+  ): Promise<Post> {
+    const post = await em.findOne(Post, { id } as FilterQuery<Post>);
+    if (!post) {
+      throw new Error('Post not found');
+    }
+    if (typeof title !== 'undefined') {
+      post.title = title;
+      await em.persistAndFlush(post);
+    }
+    return post;
+  }
+}
+0;
